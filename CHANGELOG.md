863165afb6705813016fd4ec0524e4f0271eac0e
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2025-11-24 10:54:45 +0100

    v0.6
    - Removed iov_backend: Part of project
      https://gwdg.de/en/projects/mcse/.
    - Install headers for `core`, `block_device` and `share_service`

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# v0.6
+
+## API
+
+- Removed iov_backend: Part of project https://gwdg.de/en/projects/mcse/.
+- Install headers for `core`, `block_device` and `share_service`
--- options.cmake
+++ options.cmake
@@ -7,5 +7,3 @@ set_property (CACHE MCS_CXX_STANDARD PROPERTY STRINGS "20" "23")
 option (MCS_TEST "build tests and the internal testing utilities" OFF)
 
 option (MCS_INSTALL "install the MCS" ON)
-
-option (MCS_IOV_BACKEND "compile with iov_backend" OFF)
